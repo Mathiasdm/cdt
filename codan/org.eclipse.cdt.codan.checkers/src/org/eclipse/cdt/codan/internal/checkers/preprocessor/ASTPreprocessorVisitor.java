187d4bdf4ef6abc2fa4b46a7091c010a1cb4ef91
Mathias De Mare <mathias.demare@gmail.com>
2011-08-07 16:17:35 +0200

    Added copyright statements and completed visitor pattern.

--- codan/org.eclipse.cdt.codan.checkers/src/org/eclipse/cdt/codan/internal/checkers/preprocessor/ASTPreprocessorVisitor.java
+++ codan/org.eclipse.cdt.codan.checkers/src/org/eclipse/cdt/codan/internal/checkers/preprocessor/ASTPreprocessorVisitor.java
@@ -1,10 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Mathias De Maré
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Mathias De Maré  - initial implementation
+ *******************************************************************************/
+
 package org.eclipse.cdt.codan.internal.checkers.preprocessor;
 
 import org.eclipse.cdt.core.dom.ast.ASTVisitor;
+import org.eclipse.cdt.core.dom.ast.IASTArrayModifier;
+import org.eclipse.cdt.core.dom.ast.IASTDeclSpecifier;
 import org.eclipse.cdt.core.dom.ast.IASTDeclaration;
+import org.eclipse.cdt.core.dom.ast.IASTDeclarator;
+import org.eclipse.cdt.core.dom.ast.IASTEnumerationSpecifier.IASTEnumerator;
+import org.eclipse.cdt.core.dom.ast.IASTExpression;
+import org.eclipse.cdt.core.dom.ast.IASTInitializer;
 import org.eclipse.cdt.core.dom.ast.IASTName;
 import org.eclipse.cdt.core.dom.ast.IASTNode;
+import org.eclipse.cdt.core.dom.ast.IASTParameterDeclaration;
+import org.eclipse.cdt.core.dom.ast.IASTPointerOperator;
 import org.eclipse.cdt.core.dom.ast.IASTPreprocessorStatement;
+import org.eclipse.cdt.core.dom.ast.IASTProblem;
+import org.eclipse.cdt.core.dom.ast.IASTStatement;
+import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
+import org.eclipse.cdt.core.dom.ast.IASTTypeId;
+import org.eclipse.cdt.core.dom.ast.c.ICASTDesignator;
+import org.eclipse.cdt.core.dom.ast.cpp.ICPPASTCapture;
+import org.eclipse.cdt.core.dom.ast.cpp.ICPPASTCompositeTypeSpecifier.ICPPASTBaseSpecifier;
+import org.eclipse.cdt.core.dom.ast.cpp.ICPPASTNamespaceDefinition;
+import org.eclipse.cdt.core.dom.ast.cpp.ICPPASTTemplateParameter;
 import org.eclipse.cdt.internal.core.dom.parser.ASTNode;
 import org.eclipse.cdt.internal.core.dom.rewrite.util.OffsetHelper;
 
@@ -12,7 +40,6 @@ import org.eclipse.cdt.internal.core.dom.rewrite.util.OffsetHelper;
 
 /**
  * This class adds preprocessor statements to the AST structure by visiting it.
- *
  */
 public class ASTPreprocessorVisitor extends ASTVisitor {
 	
@@ -33,20 +60,107 @@ public class ASTPreprocessorVisitor extends ASTVisitor {
 		shouldVisitParameterDeclarations = true;
 	}
 	
+	@Override
+	public int visit(IASTTranslationUnit tu) {
+		return appendPreprocessorStatements(tu);
+	}
+
+	@Override
+	public int visit(IASTInitializer initializer) {
+		return appendPreprocessorStatements(initializer);
+	}
+
+	@Override
+	public int visit(IASTParameterDeclaration parameterDeclaration) {
+		return appendPreprocessorStatements(parameterDeclaration);
+	}
+
+	@Override
+	public int visit(IASTDeclarator declarator) {
+		return appendPreprocessorStatements(declarator);
+	}
+
+	@Override
+	public int visit(IASTDeclSpecifier declSpec) {
+		return appendPreprocessorStatements(declSpec);
+	}
+
+	@Override
+	public int visit(IASTArrayModifier arrayModifier) {
+		return appendPreprocessorStatements(arrayModifier);
+	}
+
+	@Override
+	public int visit(IASTPointerOperator ptrOperator) {
+		return appendPreprocessorStatements(ptrOperator);
+	}
+
+	@Override
+	public int visit(IASTExpression expression) {
+		return appendPreprocessorStatements(expression);
+	}
+
+	@Override
+	public int visit(IASTStatement statement) {
+		return appendPreprocessorStatements(statement);
+	}
+
+	@Override
+	public int visit(IASTTypeId typeId) {
+		return appendPreprocessorStatements(typeId);
+	}
+
+	@Override
+	public int visit(IASTEnumerator enumerator) {
+		return appendPreprocessorStatements(enumerator);
+	}
+
+	@Override
+	public int visit(IASTProblem problem) {
+		return appendPreprocessorStatements(problem);
+	}
+
+	@Override
+	public int visit(ICPPASTBaseSpecifier baseSpecifier) {
+		return appendPreprocessorStatements(baseSpecifier);
+	}
+
+	@Override
+	public int visit(ICPPASTNamespaceDefinition namespaceDefinition) {
+		return appendPreprocessorStatements(namespaceDefinition);
+	}
+
+	@Override
+	public int visit(ICPPASTTemplateParameter templateParameter) {
+		return appendPreprocessorStatements(templateParameter);
+	}
+
+	@Override
+	public int visit(ICPPASTCapture capture) {
+		return appendPreprocessorStatements(capture);
+	}
+
+	@Override
+	public int visit(ICASTDesignator designator) {
+		return appendPreprocessorStatements(designator);
+	}
+	
+	@Override
+	public int visit(IASTName name) {
+		return appendPreprocessorStatements(name);
+	}
+	
+	@Override
+	public int visit(IASTDeclaration declaration) {
+		return appendPreprocessorStatements(declaration);
+	}
+
 	public ASTPreprocessorVisitor(NodePreprocessorMap map, PreprocessorHandler handler) {
 		this.map = map;
 		this.handler = handler;
 	}
 	
-	@Override
-	public int visit(IASTName name) {
-		return appendPreprocessorStatements((IASTNode) name);
-	}
 	
-	@Override
-	public int visit(IASTDeclaration declaration) {
-		return appendPreprocessorStatements((IASTNode) declaration);
-	}
 	
 	public int appendPreprocessorStatements(IASTNode node) {
 		while(handler.hasMore()) {
