65b47a57f4a5a183ba95a8f8252d515be7c6867c
Mathias De Mare <mathias.demare@gmail.com>
2011-08-07 17:08:38 +0200

    Revert "Improve state machine for candidate issues."
    This reverts commit 571d947499c3603edc7d8e570d7960ca0b1276dc.

--- codan/org.eclipse.cdt.codan.checkers/src/org/eclipse/cdt/codan/internal/checkers/UnneededHeaderGuard.java
+++ codan/org.eclipse.cdt.codan.checkers/src/org/eclipse/cdt/codan/internal/checkers/UnneededHeaderGuard.java
@@ -96,7 +96,7 @@ public class UnneededHeaderGuard extends AbstractIndexAstChecker {
 	 * The possible states when trying to detect an issue.
 	 */
 	private enum PositionState {
-		START, IFNDEF, INCLUDE
+		START, IFNDEF, INCLUDE, ENDIF
 	}
 	
 	/**
@@ -128,9 +128,15 @@ public class UnneededHeaderGuard extends AbstractIndexAstChecker {
 					break;
 				case INCLUDE:
 					if(statement instanceof IASTPreprocessorEndifStatement) {
-						candidateIssues.add(new CandidateIssue(define, include));
+						state = PositionState.ENDIF;
 					}
-					state = PositionState.START;
+					else {
+						state = PositionState.START;
+					}
+			}
+			if(state == PositionState.ENDIF) {
+				candidateIssues.add(new CandidateIssue(define, include));
+				state = PositionState.START;
 			}
 		}
 		return candidateIssues;
