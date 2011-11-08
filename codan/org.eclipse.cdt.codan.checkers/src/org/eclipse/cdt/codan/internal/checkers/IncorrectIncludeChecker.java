2e562dcf41fdfcfeaf2bc2aef3c84c70c3c5a2fe
Mathias De Maré <mathias.demare@gmail.com>
2011-11-08 18:12:58 +0100

    Start building include structure.

--- codan/org.eclipse.cdt.codan.checkers/src/org/eclipse/cdt/codan/internal/checkers/IncorrectIncludeChecker.java
+++ codan/org.eclipse.cdt.codan.checkers/src/org/eclipse/cdt/codan/internal/checkers/IncorrectIncludeChecker.java
@@ -1,11 +1,75 @@
 package org.eclipse.cdt.codan.internal.checkers;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.eclipse.cdt.codan.core.cxx.model.AbstractIndexAstChecker;
+import org.eclipse.cdt.codan.internal.checkers.IncorrectIncludeChecker.IncludeGraphEntry;
+import org.eclipse.cdt.core.dom.ast.ASTVisitor;
+import org.eclipse.cdt.core.dom.ast.IASTPreprocessorIncludeStatement;
+import org.eclipse.cdt.core.dom.ast.IASTPreprocessorStatement;
 import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
 
 public class IncorrectIncludeChecker extends AbstractIndexAstChecker {
 	
 	public static final String ER_ID = "org.eclipse.cdt.codan.internal.checkers.IncorrectIncludeChecker"; //$NON-NLS-1$
+	
+	private Map<String, IncludeGraphEntry> includeStructure = new HashMap<String, IncludeGraphEntry>();
+	
+	class IncludeGraphEntry {
+		
+		private List<IncludeGraphEntry> referencedIncludes;
+		private String name;
+		
+		public IncludeGraphEntry(String name) {
+			this.name = name;
+			referencedIncludes = new ArrayList<IncludeGraphEntry>();
+		}
+		
+		public void addReferencedIncludes() {
+			//TODO
+		}
+	}
+	
+	public List<IncludeGraphEntry> getGraphEntries(IASTPreprocessorStatement[] statements) {
+		//TODO
+	}
+	
+	class IncludeASTVisitor extends ASTVisitor {
+		public IncludeASTVisitor(Map<String, IncludeGraphEntry> includeStructure) {
+			shouldVisitTranslationUnit = true;
+		}
+		
+		public int visit(IASTTranslationUnit tu) {
+			if(includeStructure.containsKey(tu.getContainingFilename())) {
+				//Already processed
+				return PROCESS_CONTINUE;
+			}
+			
+			//Add entry
+			IncludeGraphEntry entry = new IncludeGraphEntry(tu.getContainingFilename());
+			includeStructure.put(tu.getContainingFilename(), entry);
+			
+			//Add all entries referenced from this entry
+			for(IASTPreprocessorStatement statement: tu.getAllPreprocessorStatements()) {
+				if(!(statement instanceof IASTPreprocessorIncludeStatement)) {
+					continue;
+				}
+				
+				String path = ((IASTPreprocessorIncludeStatement) statement).getPath();
+				if(path == null) {
+					continue;
+				}
+				
+				//TODO: add statement to containing IncludeGraphEntry
+				//TODO: add statement to includeStructure
+			}
+			
+			return PROCESS_CONTINUE;
+		}
+	}
 	
 	public void processAst(IASTTranslationUnit ast) {
 		/*
@@ -17,6 +81,9 @@ public class IncorrectIncludeChecker extends AbstractIndexAstChecker {
 		 *     * Files directly included but _not_ used
 		 *     * Files used but _not_ directly included
 		 */
+		
+		ast.accept(new IncludeASTVisitor(includeStructure));
 		System.out.println(ast.getContainingFilename());
+		//ast.getAllPreprocessorStatements()
 	}
 }
