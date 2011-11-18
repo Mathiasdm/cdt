45994ac520e8e26b24561e57342803c9ecadb435
Mathias De Maré <mathias.demare@gmail.com>
2011-11-18 18:34:21 +0100

    codan: clean up headerguard to not rely on other files.

--- codan/org.eclipse.cdt.codan.checkers/src/org/eclipse/cdt/codan/internal/checkers/UnneededHeaderGuard.java
+++ codan/org.eclipse.cdt.codan.checkers/src/org/eclipse/cdt/codan/internal/checkers/UnneededHeaderGuard.java
@@ -11,8 +11,6 @@
 
 package org.eclipse.cdt.codan.internal.checkers;
 
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -21,26 +19,16 @@ import org.eclipse.cdt.codan.core.cxx.model.AbstractIndexAstChecker;
 import org.eclipse.cdt.codan.internal.checkers.preprocessor.ASTPreprocessorVisitor;
 import org.eclipse.cdt.codan.internal.checkers.preprocessor.NodePreprocessorMap;
 import org.eclipse.cdt.codan.internal.checkers.preprocessor.PreprocessorHandler;
-import org.eclipse.cdt.core.dom.ast.IASTNode;
-import org.eclipse.cdt.core.dom.ast.IASTPreprocessorElifStatement;
-import org.eclipse.cdt.core.dom.ast.IASTPreprocessorElseStatement;
 import org.eclipse.cdt.core.dom.ast.IASTPreprocessorEndifStatement;
-import org.eclipse.cdt.core.dom.ast.IASTPreprocessorIfStatement;
-import org.eclipse.cdt.core.dom.ast.IASTPreprocessorIfdefStatement;
 import org.eclipse.cdt.core.dom.ast.IASTPreprocessorIfndefStatement;
 import org.eclipse.cdt.core.dom.ast.IASTPreprocessorIncludeStatement;
-import org.eclipse.cdt.core.dom.ast.IASTPreprocessorMacroDefinition;
 import org.eclipse.cdt.core.dom.ast.IASTPreprocessorStatement;
 import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
-import org.eclipse.cdt.core.model.CModelException;
-import org.eclipse.cdt.core.model.CoreModelUtil;
-import org.eclipse.cdt.core.model.ITranslationUnit;
-import org.eclipse.core.runtime.CoreException;
 
 /**
- * Checks if unneeded external header guards are present.
+ * Checks if external header guards are present.
  * <p>
- * An external header guard is unneeded if an included header already has guards itself.
+ * This should not be present, since well-formed header files should already have internal header guards.
  * @author Mathias De Maré
  *
  */
@@ -57,37 +45,32 @@ public class UnneededHeaderGuard extends AbstractIndexAstChecker {
 		ASTPreprocessorVisitor visitor = new ASTPreprocessorVisitor(map, handler);
 		//Visit AST to add preprocessor statements to map
 		ast.accept(visitor);
+		ArrayList<ArrayList<IASTPreprocessorStatement>> statementGroups = 
+				new ArrayList<ArrayList<IASTPreprocessorStatement>>(map.getMap().values());
+		statementGroups.add(handler.getList()); //Add remaining issues
 		
-		//Determine candidate issues
-		List<CandidateIssue> candidateIssues = new ArrayList<CandidateIssue>();
-		for(IASTNode key: map.getMap().keySet()) { //Append issues related to nodes
-			ArrayList<IASTPreprocessorStatement> statements = map.getMap().get(key);
-			List<CandidateIssue> candidateIssuesForNode = getCandidateIssues(statements);
-			candidateIssues.addAll(candidateIssuesForNode);
-		}
-		candidateIssues.addAll(getCandidateIssues(handler.getList())); //Append issues that are not before any node
-		
-		//Check if the candidate issues are actual issues
-		for(CandidateIssue candidate: candidateIssues) {
-			if(isIssue(candidate)) {
-				reportProblem(ER_ID, candidate.getInclude());
+		//Determine issues
+		for(ArrayList<IASTPreprocessorStatement> statements: statementGroups) {
+			List<Issue> issues = getIssues(statements);
+			for(Issue issue: issues) {
+				reportProblem(ER_ID, issue.getInclude());
 			}
 		}
 	}
 	
 	/**
-	 * Potential unneeded header guard.
+	 * External header guard.
 	 */
-	private class CandidateIssue {
+	private class Issue {
 		private String define;
 		private IASTPreprocessorIncludeStatement include;
 		
 		/**
-		 * Construct a candidate issue
+		 * Construct a headerguard issue
 		 * @param define The String checked in the '#ifndef' statement.
 		 * @param include The include statement surrounded by header guards.
 		 */
-		public CandidateIssue(String define, IASTPreprocessorIncludeStatement include) {
+		public Issue(String define, IASTPreprocessorIncludeStatement include) {
 			this.define = define;
 			this.include = include;
 		}
@@ -109,11 +92,10 @@ public class UnneededHeaderGuard extends AbstractIndexAstChecker {
 	}
 	
 	/**
-	 * Go through the preprocessor statements that directory follow each other, and check
-	 * if there are any potential issues.
+	 * Go through the preprocessor statements that directory follow each other, and check if there are any issues.
 	 */
-	List<CandidateIssue> getCandidateIssues(ArrayList<IASTPreprocessorStatement> statements) {
-		ArrayList<CandidateIssue> candidateIssues = new ArrayList<CandidateIssue>();
+	List<Issue> getIssues(ArrayList<IASTPreprocessorStatement> statements) {
+		ArrayList<Issue> issues = new ArrayList<Issue>();
 		
 		PositionState state = PositionState.START;
 		String define = null;
@@ -137,173 +119,12 @@ public class UnneededHeaderGuard extends AbstractIndexAstChecker {
 					break;
 				case INCLUDE:
 					if(statement instanceof IASTPreprocessorEndifStatement) {
-						candidateIssues.add(new CandidateIssue(define, include));
+						issues.add(new Issue(define, include));
 					}
 					state = PositionState.START;
 					break;
 			}
 		}
-		return candidateIssues;
-	}
-	
-	/**
-	 * Check if the potential issue is an actual issue by checking the included header.
-	 */
-	private boolean isIssue(CandidateIssue candidate) {
-		IASTTranslationUnit ast = getASTForCandidateIssue(candidate);
-		
-		if(ast == null) {
-			return false; //We can't generate an AST, so no way to make sure the issue is real
-		}
-
-		ArrayList<IASTPreprocessorStatement> preprocessorStatements =
-				new ArrayList<IASTPreprocessorStatement>(
-						Arrays.asList(ast.getAllPreprocessorStatements()));
-		PreprocessorHandler handler = new PreprocessorHandler(preprocessorStatements);
-		NodePreprocessorMap map = new NodePreprocessorMap();
-
-		ASTPreprocessorVisitor visitor = new ASTPreprocessorVisitor(map, handler);
-
-		//Add preprocessor statements
-		ast.accept(visitor);
-		
-		//We need at least #ifndef, #define and #endif
-		if(preprocessorStatements.size() < 3) {
-			return false;
-		}
-		
-		//The 2 first and the last statement should be #ifndef, #define and #endif and use the correct define string
-		if(!isFirstPreprocessorStatementCorrect(preprocessorStatements.get(0), candidate.getDefine())
-				|| !isSecondPreprocessorStatementCorrect(preprocessorStatements.get(1), candidate.getDefine())
-				|| !isLastPreprocessorStatementCorrect(preprocessorStatements.get(preprocessorStatements.size() - 1))) {
-			return false;
-		}
-
-		//Only the first #ifndef and last #endif should be at 'level 0'. All other preprocessor statements should be inside these 2.
-		if(!isToplevelStatementCountCorrect(preprocessorStatements)) {
-			return false;
-		}
-
-		IASTNode[] nodes = ast.getChildren();
-		if(nodes.length > 0) {
-			//#ifndef and #define need to be before the first node
-			IASTNode node = nodes[0];
-			ArrayList<IASTPreprocessorStatement> statements = map.getMap().get(node);
-			if((statements == null) || (statements.size() < 2)) {
-				return false;
-			}
-			//We need to have preprocessor statements at the end (to make sure the last '#endif' is below any IASTNode)
-			if(!handler.hasMore()) {
-				return false;
-			}
-		}
-
-		return true; //All checks passed
-	}
-
-	/**
-	 * Check if the amount of top level preprocessor statements is correct
-	 * (only the first '#ifndef' and the last '#endif' can be at the top level).
-	 */
-	private boolean isToplevelStatementCountCorrect(ArrayList<IASTPreprocessorStatement> preprocessorStatements) {
-		int indentationLevel = 0;
-		int statementsAtLowestLevel = 0;
-		for(int i=0; i<preprocessorStatements.size(); i++) {
-			IASTPreprocessorStatement statement = preprocessorStatements.get(i);
-
-			if(statement instanceof IASTPreprocessorElifStatement
-					|| statement instanceof IASTPreprocessorElseStatement
-					|| statement instanceof IASTPreprocessorEndifStatement) {
-				indentationLevel--;
-			}
-
-			if(indentationLevel == 0) {
-				statementsAtLowestLevel++;
-			}
-
-			if(statement instanceof IASTPreprocessorIfndefStatement 
-					|| statement instanceof IASTPreprocessorIfdefStatement
-					|| statement instanceof IASTPreprocessorIfStatement
-					|| statement instanceof IASTPreprocessorElifStatement
-					|| statement instanceof IASTPreprocessorElseStatement) {
-				indentationLevel++;
-			}
-		}
-
-		if(statementsAtLowestLevel != 2) {
-			return false;
-		}
-		return true;
-	}
-
-	/**
-	 * Get the AST for this potential issue. If it is not possible to find the AST, null is returned.
-	 */
-	private IASTTranslationUnit getASTForCandidateIssue(CandidateIssue candidate) {
-		IASTPreprocessorIncludeStatement include = candidate.getInclude();
-		
-		if(!include.isResolved()) {
-			//We cannot find the include, so we can't evaluate it
-			return null;
-		}
-		
-		String path = include.getPath();
-		
-		try {
-			ITranslationUnit tu = CoreModelUtil.findTranslationUnitForLocation(new URI(path), null);
-			if(tu == null) {
-				return null; //Didn't find translation unit
-			}
-			IASTTranslationUnit ast = tu.getAST();
-			return ast;
-		} catch (CModelException e) {
-			return null; //Failed to find a translation unit
-		} catch (URISyntaxException e) {
-			return null; //The path has incorrect syntax
-		} catch (CoreException e) {
-			return null; //Failed to generate the AST
-		}
-	}
-	
-	/**
-	 * Check if the first preprocessor statement in the included header is correct.
-	 * It needs to be an #ifndef and needs to have to correct define string.
-	 */
-	private boolean isFirstPreprocessorStatementCorrect(IASTPreprocessorStatement statement, String define) {
-		if(!(statement instanceof IASTPreprocessorIfndefStatement)) {
-			return false;
-		}
-
-		String condition = new String(((IASTPreprocessorIfndefStatement) statement).getCondition());
-		if(!define.equals(condition)) {
-			return false;
-		}
-		return true;
-	}
-	
-	/**
-	 * Check if the second preprocessor statement in the included header is correct.
-	 * It needs to be an #define and needs to define the correct string.
-	 */
-	private boolean isSecondPreprocessorStatementCorrect(IASTPreprocessorStatement statement, String define) {
-		if(!(statement instanceof IASTPreprocessorMacroDefinition)) {
-			return false;
-		}
-
-		IASTPreprocessorMacroDefinition macroDefinition = (IASTPreprocessorMacroDefinition) statement;
-		if(!define.equals(macroDefinition.getName().toString())) {
-			return false;
-		}
-		return true;
-	}
-	
-	/**
-	 * The last preprocessor statement needs to be an #endif
-	 */
-	private boolean isLastPreprocessorStatementCorrect(IASTPreprocessorStatement statement) {
-		if(!(statement instanceof IASTPreprocessorEndifStatement)) {
-			return false;
-		}
-		return true;
+		return issues;
 	}
 }
--- codan/org.eclipse.cdt.codan.core.test/src/org/eclipse/cdt/codan/core/internal/checkers/UnneededHeaderGuardTest.java
+++ codan/org.eclipse.cdt.codan.core.test/src/org/eclipse/cdt/codan/core/internal/checkers/UnneededHeaderGuardTest.java
@@ -10,204 +10,243 @@
  *******************************************************************************/
 
 package org.eclipse.cdt.codan.core.internal.checkers;
-import java.io.File;
-
 import org.eclipse.cdt.codan.core.test.CheckerTestCase;
 
 @SuppressWarnings("unused")
 public class UnneededHeaderGuardTest extends CheckerTestCase {
 
-	// @file:includedheader.h
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+		enableProblems("org.eclipse.cdt.codan.internal.checkers.UnneededHeaderGuard"); //$NON-NLS-1$
+	}
+
+	// @file:header.h
+	// #ifndef HEADER_H
+	// #define HEADER_H
 	// #ifndef ABC_H
-	// #define ABC_H
+	// #include "included.h"
+	// #endif
+	// #endif
+	public void testUnneededSingleQuotedWithoutSurroundings() {
+		loadCodeAndRun(getAboveComment());
+		checkErrorLine(4);
+	}
+
+	// @file:impl.cpp
+	// #ifndef DEF_H
+	// #include <someheaderfile_blah_fooBar.hpp>
+	// #endif
+	public void testUnneededSingleBracketsWithoutSurroundings() {
+		loadCodeAndRun(getAboveComment());
+		checkErrorLine(2);
+	}
+
+	// @file:foo.c
 	// int foo();
+	// #ifndef FOOFOO_H
+	// #include "foofoo.hh"
 	// #endif
-	/* ---- */
-	// @file:header.h
-	// #ifndef ABC_H
-	// #include "includedheader.h" //Warning
-	// #endif
-	// int blah();
-	public void testSingleUnneededGuard() {
-		StringBuilder[] code = getContents(2);
-		File f1 = loadcode(code[0].toString());
-		File f2 = loadcode(code[1].toString());
-		runOnProject();
-		checkErrorLine(f2, 2);
+	public void testUnneededSingleQuotedBehindCode() {
+		loadCodeAndRun(getAboveComment());
+		checkErrorLine(3);
 	}
 
-	// @file:includedheader.h
-	// int i=1;
-	// #ifndef DEF_H //Internal header guards are at the end, check if they still work correctly
-	// #define DEF_H
-	// int foo();
+	// @file:randomname.hpp
+	// #ifndef SOME_MESSAGE_H
+	// #include "someMessage.hpp"
 	// #endif
-	/* ---- */
-	// @file:header.h
-	// #ifndef DEF_H
-	// #include "includedheader.h" //No warning
-	// #endif
-	public void testSingleNeededGuardAtEnd() {
-		StringBuilder[] code = getContents(2);
-		File f1 = loadcode(code[0].toString());
-		File f2 = loadcode(code[1].toString());
-		runOnProject();
-		checkNoErrors();
-	}
-
-	// @file:includedheader.h
-	// int i=1; //Statement outside of the guards causes external guards to be needed
-	// #ifndef DEF_H
-	// #define DEF_H
-	// int foo();
-	// #endif
-	/* ---- */
-	// @file:header.h
-	// #ifndef DEF_H
-	// #include "includedheader.h" //No warning
-	// #endif
-	// int bar();
-	public void testSingleNeededGuard() {
-		StringBuilder[] code = getContents(2);
-		File f1 = loadcode(code[0].toString());
-		File f2 = loadcode(code[1].toString());
-		runOnProject();
-		checkNoErrors();
-	}
-
-	// @file:includedheader.h
-	// #ifndef DEF_H
-	// #define DEF_H
-	// int foo();
-	// #else
-	// //do stuff here
-	// #endif
-	/* ---- */
-	// @file:header.h
-	// #ifndef DEF_H
-	// #include "includedheader.h" //No warning
-	// #endif
-	// int bar();
-	public void testNeededGuardDueToElse() {
-		StringBuilder[] code = getContents(2);
-		File f1 = loadcode(code[0].toString());
-		File f2 = loadcode(code[1].toString());
-		runOnProject();
-		checkNoErrors();
-	}
-
-	// @file:includedheader.h
-	// #ifndef DEF_H
-	// #define DEF_H
-	// int foo();
-	// #endif
-	/* ---- */
-	// @file:header.h
-	// #ifndef DEF_H
-	// #include "includedheader.h" //Warning
-	// #endif
-	public void testUnneededGuardWithoutOtherStatements() {
-		StringBuilder[] code = getContents(2);
-		File f1 = loadcode(code[0].toString());
-		File f2 = loadcode(code[1].toString());
-		runOnProject();
-		checkErrorLine(f2, 2);
-	}
-
-	// @file:a.h
-	// #ifndef A
-	// #define A
-	// void a();
-	// #endif
-	/* ---- */
-	// @file:b.h
-	// #ifndef B
-	// #define B
-	// void b();
-	// #endif
-	/* ---- */
-	// @file:header.h
-	// #ifndef A
-	// #include "a.h" //Warning
-	// #endif
-	//
-	// #ifndef B
-	// #include "b.h" //Warning
-	// #endif
-	public void testMultipleUnneededGuards() {
-		StringBuilder[] code = getContents(3);
-		File f1 = loadcode(code[0].toString());
-		File f2 = loadcode(code[1].toString());
-		File f3 = loadcode(code[2].toString());
-		runOnProject();
-		checkErrorLine(f3, 2);
-		checkErrorLine(f3, 6);
-	}
-
-	//@file:included.hpp
-	// #ifndef INCLUDED_H
-	// #define INCLUDED_H
-	// #endif
-	// i++; //Statement after the header guards: redundant guards are needed.
-	/* ---- */
-	//@file:header.hpp
+	// void bar() {
 	// int i = 1;
-	// #ifndef INCLUDED_H
-	// #include "included.hpp" //No warning
-	// #endif
-	public void testStatementAfterGuards() {
-		StringBuilder[] code = getContents(2);
-		File f1 = loadcode(code[0].toString());
-		File f2 = loadcode(code[1].toString());
-		runOnProject();
-		checkNoErrors();
-	}
-
-	//@file:included.hpp
-	// #ifndef INCLUDED_HPP
-	// #define INCLUDED_HPP
-	// #ifndef INCLUDED_INTERNAL
-	// #define INCLUDED_INTERNAL
-	// #endif
-	// int foo();
-	// #endif
-	/* ---- */
-	//@file:header.hpp
-	// int bar();
-	// char* foobar();
-	// #ifndef INCLUDED_HPP
-	// #include "included.hpp" //Warning
-	// #endif
-	//
-	public void testNestedPreprocessorStatements() {
-		StringBuilder[] code = getContents(2);
-		File f1 = loadcode(code[0].toString());
-		File f2 = loadcode(code[1].toString());
-		runOnProject();
-		checkErrorLine(f2, 4);
-	}
-
-	//@file:included.hpp
-	// #ifndef ABC_H
-	// #define ABC_H
-	// void foo(int, char*);
-	// #endif
-	// #ifndef DEF_H
-	// #define DEF_H
-	// int bar();
-	// #endif
-	/* ---- */
-	//@file:header.hpp
-	// #ifndef ABC_H
-	// #include "included.hpp" //No warning: 2 sequential header guards don't cover the entire file
-	// #endif
-	public void testSequentialPreprocessorStatements() {
-		StringBuilder[] code = getContents(2);
-		File f1 = loadcode(code[0].toString());
-		File f2 = loadcode(code[1].toString());
-		runOnProject();
-		checkNoErrors();
+	// }
+	public void testUnneededSingleQuotedBeforeCode() {
 
 	}
 
+	public void testUnneededSingleQuotedBetweenCode() {
+
+	}
+
+	public void testIncompleteSingleQuoted() {
+
+	}
+
+	public void testIntermingledSingleQuoted() {
+
+	}
+
+//	// @file:includedheader.h
+//	// int i=1;
+//	// #ifndef DEF_H //Internal header guards are at the end, check if they still work correctly
+//	// #define DEF_H
+//	// int foo();
+//	// #endif
+//	/* ---- */
+//	// @file:header.h
+//	// #ifndef DEF_H
+//	// #include "includedheader.h" //No warning
+//	// #endif
+//	public void testSingleNeededGuardAtEnd() {
+//		StringBuilder[] code = getContents(2);
+//		File f1 = loadcode(code[0].toString());
+//		File f2 = loadcode(code[1].toString());
+//		runOnProject();
+//		checkNoErrors();
+//	}
+//
+//	// @file:includedheader.h
+//	// int i=1; //Statement outside of the guards causes external guards to be needed
+//	// #ifndef DEF_H
+//	// #define DEF_H
+//	// int foo();
+//	// #endif
+//	/* ---- */
+//	// @file:header.h
+//	// #ifndef DEF_H
+//	// #include "includedheader.h" //No warning
+//	// #endif
+//	// int bar();
+//	public void testSingleNeededGuard() {
+//		StringBuilder[] code = getContents(2);
+//		File f1 = loadcode(code[0].toString());
+//		File f2 = loadcode(code[1].toString());
+//		runOnProject();
+//		checkNoErrors();
+//	}
+//
+//	// @file:includedheader.h
+//	// #ifndef DEF_H
+//	// #define DEF_H
+//	// int foo();
+//	// #else
+//	// //do stuff here
+//	// #endif
+//	/* ---- */
+//	// @file:header.h
+//	// #ifndef DEF_H
+//	// #include "includedheader.h" //No warning
+//	// #endif
+//	// int bar();
+//	public void testNeededGuardDueToElse() {
+//		StringBuilder[] code = getContents(2);
+//		File f1 = loadcode(code[0].toString());
+//		File f2 = loadcode(code[1].toString());
+//		runOnProject();
+//		checkNoErrors();
+//	}
+//
+//	// @file:includedheader.h
+//	// #ifndef DEF_H
+//	// #define DEF_H
+//	// int foo();
+//	// #endif
+//	/* ---- */
+//	// @file:header.h
+//	// #ifndef DEF_H
+//	// #include "includedheader.h" //Warning
+//	// #endif
+//	public void testUnneededGuardWithoutOtherStatements() {
+//		StringBuilder[] code = getContents(2);
+//		File f1 = loadcode(code[0].toString());
+//		File f2 = loadcode(code[1].toString());
+//		runOnProject();
+//		checkErrorLine(f2, 2);
+//	}
+//
+//	// @file:a.h
+//	// #ifndef A
+//	// #define A
+//	// void a();
+//	// #endif
+//	/* ---- */
+//	// @file:b.h
+//	// #ifndef B
+//	// #define B
+//	// void b();
+//	// #endif
+//	/* ---- */
+//	// @file:header.h
+//	// #ifndef A
+//	// #include "a.h" //Warning
+//	// #endif
+//	//
+//	// #ifndef B
+//	// #include "b.h" //Warning
+//	// #endif
+//	public void testMultipleUnneededGuards() {
+//		StringBuilder[] code = getContents(3);
+//		File f1 = loadcode(code[0].toString());
+//		File f2 = loadcode(code[1].toString());
+//		File f3 = loadcode(code[2].toString());
+//		runOnProject();
+//		checkErrorLine(f3, 2);
+//		checkErrorLine(f3, 6);
+//	}
+//
+//	//@file:included.hpp
+//	// #ifndef INCLUDED_H
+//	// #define INCLUDED_H
+//	// #endif
+//	// i++; //Statement after the header guards: redundant guards are needed.
+//	/* ---- */
+//	//@file:header.hpp
+//	// int i = 1;
+//	// #ifndef INCLUDED_H
+//	// #include "included.hpp" //No warning
+//	// #endif
+//	public void testStatementAfterGuards() {
+//		StringBuilder[] code = getContents(2);
+//		File f1 = loadcode(code[0].toString());
+//		File f2 = loadcode(code[1].toString());
+//		runOnProject();
+//		checkNoErrors();
+//	}
+//
+//	//@file:included.hpp
+//	// #ifndef INCLUDED_HPP
+//	// #define INCLUDED_HPP
+//	// #ifndef INCLUDED_INTERNAL
+//	// #define INCLUDED_INTERNAL
+//	// #endif
+//	// int foo();
+//	// #endif
+//	/* ---- */
+//	//@file:header.hpp
+//	// int bar();
+//	// char* foobar();
+//	// #ifndef INCLUDED_HPP
+//	// #include "included.hpp" //Warning
+//	// #endif
+//	//
+//	public void testNestedPreprocessorStatements() {
+//		StringBuilder[] code = getContents(2);
+//		File f1 = loadcode(code[0].toString());
+//		File f2 = loadcode(code[1].toString());
+//		runOnProject();
+//		checkErrorLine(f2, 4);
+//	}
+//
+//	//@file:included.hpp
+//	// #ifndef ABC_H
+//	// #define ABC_H
+//	// void foo(int, char*);
+//	// #endif
+//	// #ifndef DEF_H
+//	// #define DEF_H
+//	// int bar();
+//	// #endif
+//	/* ---- */
+//	//@file:header.hpp
+//	// #ifndef ABC_H
+//	// #include "included.hpp" //No warning: 2 sequential header guards don't cover the entire file
+//	// #endif
+//	public void testSequentialPreprocessorStatements() {
+//		StringBuilder[] code = getContents(2);
+//		File f1 = loadcode(code[0].toString());
+//		File f2 = loadcode(code[1].toString());
+//		runOnProject();
+//		checkNoErrors();
+//
+//	}
+//
 }
